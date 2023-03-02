817ee30410a33eefc9400b341bfa4de2730f36ba
Rachel Lam <rachellam@Rachels-MacBook-Pro.local>
2023-03-02 17:08:02 +0900

    feat: add router:

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,19 @@
 import './App.scss';
-import { TodoPage, LoginPage, SignUpPage } from './pages';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { HomePage, TodoPage, LoginPage, SignUpPage } from './pages';
 
 function App() {
   return (
     <div className="app">
-      <TodoPage />
+      {/* <TodoPage /> */}
+      <BrowserRouter>
+        <Routes>
+          <Route path="login" element={<LoginPage />} />
+          <Route path="signup" element={<SignUpPage />} />
+          <Route path="todo" element={<TodoPage />} />
+          <Route path="*" element={<HomePage />} />
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
