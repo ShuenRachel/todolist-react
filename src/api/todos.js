d79e6989352ecd8cdb53cdb71cd68b074908afd2
Rachel Lam <rachellam@Rachels-MacBook-Pro.local>
2023-03-03 11:00:36 +0900

    setup axios

---
+++ src/api/todos.js
@@ -0,0 +1,33 @@
+import axios from 'axios';
+
+const baseUrl = 'http://localhost:3001';
+
+export async function getTodos() {
+  try {
+    const res = await axios.get(`${baseUrl}/todos`);
+    return res.data;
+  } catch (e) {
+    console.error('[Get Todos failed]:', e);
+  }
+}
+export async function createTodo(payload) {
+  try {
+    const { title, isDone } = payload;
+    const res = await axios.post(`${baseUrl}/todos`, { title, isDone });
+    return res.data;
+  } catch (e) {
+    console.error('[Get Todos failed]:', e);
+  }
+}
+export async function patchTodo() {
+  try {
+  } catch (e) {
+    console.error('[Get Todos failed]:', e);
+  }
+}
+export async function deleteTodo() {
+  try {
+  } catch (e) {
+    console.error('[Get Todos failed]:', e);
+  }
+}
