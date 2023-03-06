d5f2a05e1fdffc609b269ca28a16c85b35391bf7
Rachel Lam <rachellam@Rachels-MacBook-Pro.local>
2023-03-06 11:53:14 +0900

    feat: setup login page

--- src/pages/LoginPage.jsx
+++ src/pages/LoginPage.jsx
@@ -6,8 +6,16 @@ import {
 } from 'components/common/auth.styled';
 import { ACLogoIcon } from 'assets/images';
 import { AuthInput } from 'components';
+import { useState } from 'react';
 
 const LoginPage = () => {
+  const [userName, setUserName] = useState('');
+  const [password, setPassword] = useState('');
+
+  function handleUsernameInput(userNameInput) {}
+
+  function handlePasswordInput(passwordInput) {}
+
   return (
     <AuthContainer>
       <div>
@@ -16,11 +24,22 @@ const LoginPage = () => {
       <h1>登入 Todo</h1>
 
       <AuthInputContainer>
-        <AuthInput />
+        <AuthInput
+          label="Username"
+          value={userName}
+          placeholder="Please input usename"
+          onChange={(userNameInput) => handleUsernameInput(userNameInput)}
+        />
       </AuthInputContainer>
 
       <AuthInputContainer>
-        <AuthInput />
+        <AuthInput
+          type="password"
+          label="Password"
+          value={password}
+          placeholder="Please input password"
+          onChange={(passwordInput) => handlePasswordInput(passwordInput)}
+        />
       </AuthInputContainer>
       <AuthButton>登入</AuthButton>
       <AuthLinkText>註冊</AuthLinkText>
